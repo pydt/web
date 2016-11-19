7c153f3bcebf4fe1fda4200ba3d761ec943ec95f
Mike Rosack <mrosack@gmail.com>
2016-11-19 09:32:28 -0600

    forum iframe positioning tweak

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-forum',
-  templateUrl: './forum.component.html',
-  styleUrls: ['./forum.component.scss']
+  templateUrl: './forum.component.html'
 })
 export class ForumComponent implements OnInit {
 
