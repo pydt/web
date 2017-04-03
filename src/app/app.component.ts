94367221d7eaa4bd0b48f3e15221651c9239bcf5
Mike Rosack <mrosack@gmail.com>
2017-04-02 21:24:37 -0500

    notify user to reload page when a new build has been deployed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ErrorHandler, OnInit, ViewChild, ViewContainerRef  } from '@angular/core';
-import { Response } from '@angular/http';
+import { Response, Http } from '@angular/http';
 import { Router } from '@angular/router';
 import { Angulartics2GoogleAnalytics } from 'angulartics2';
 import { ModalDirective } from 'ng2-bootstrap/ng2-bootstrap';
@@ -50,11 +50,15 @@ export class AppComponent implements OnInit {
   private busy: Promise<any>;
   private errorModalMessage: string;
   private alerts: AlertConfig[] = [];
+  private updateInterval: any;
+  private lastIndexHash?: number;
 
   @ViewChild('errorModal') errorModal: ModalDirective;
+  @ViewChild('updateModal') updateModal: ModalDirective;
 
   constructor(
     private api: ApiService,
+    private http: Http,
     private errorService: ErrorHandler,
     private router: Router,
     private metaService: MetaService,
@@ -64,6 +68,13 @@ export class AppComponent implements OnInit {
   ) {
     // Angular2 Bootstrap hack: https://valor-software.com/ng2-bootstrap/#/modals
     this.viewContainerRef = viewContainerRef;
+
+    this.updateInterval = setInterval(() => {
+      // Check for app update once a minute
+      this.checkForAppUpdate();
+    }, 60 * 1000);
+
+    this.checkForAppUpdate();
   }
 
   ngOnInit() {
@@ -98,6 +109,51 @@ export class AppComponent implements OnInit {
         Rollbar.error(next);
       }
     });
+  }
+
+  checkForAppUpdate() {
+    this.http.get('/index.html').subscribe(resp => {
+      if (resp.status === 200) {
+        if (!this.lastIndexHash) {
+          this.lastIndexHash = this.hash(resp.text());
+        } else {
+          const curHash = this.hash(resp.text());
+
+          if (this.lastIndexHash !== curHash) {
+            clearInterval(this.updateInterval);
+            this.updateModal.show();
+          }
+
+          this.lastIndexHash = curHash;
+        }
+      }
+    }, err => {
+      console.log(`Couldn't check for app update...`);
+    });
+  }
+
+  reload() {
+    window.location.reload(true);
+  }
+
+  /**
+   * Java-like hashCode function for strings
+   *
+   * taken from http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery/7616484#7616484
+   */
+  private hash(str) {
+    const len = str.length;
+    let hash = 0;
+    if (len === 0) {
+      return hash;
+    }
+
+    let i;
+    for (i = 0; i < len; i++) {
+      hash = ((hash << 5) - hash) + str.charCodeAt(i);
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
   }
 
   updateIsLoggedIn() {
