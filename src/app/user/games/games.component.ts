23923f83d9b246b6aac5de76160e7109ed1de01d
Mike Rosack <mike@rosacksoftwaresolutions.com>
2020-10-17 05:47:02 -0500

    sort current turn games to top, add completed games view

--- src/app/user/games/games.component.ts
+++ src/app/user/games/games.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Game, ProfileCacheService, UserService } from 'pydt-shared';
+import { AuthService } from '../../shared/auth.service';
 
 @Component({
   selector: 'pydt-user-games',
@@ -7,8 +8,9 @@ import { Game, ProfileCacheService, UserService } from 'pydt-shared';
 })
 export class UserGamesComponent implements OnInit {
   games: Game[];
+  completedGames: Game[];
 
-  constructor(private userApi: UserService, private profileCache: ProfileCacheService) {
+  constructor(private userApi: UserService, private authService: AuthService, private profileCache: ProfileCacheService) {
   }
 
   ngOnInit() {
@@ -19,7 +21,23 @@ export class UserGamesComponent implements OnInit {
     this.userApi.games().subscribe(resp => {
       // Go ahead and get all profiles for all the games in one request
       this.profileCache.getProfilesForGames(resp.data);
-      this.games = resp.data;
+
+      const profile = this.authService.getSteamProfile();
+
+      const yourTurnGames = resp.data.filter((game: Game) => {
+        return game.inProgress && game.currentPlayerSteamId === profile.steamid;
+      });
+
+      this.games = [
+        ...yourTurnGames,
+        ...resp.data.filter(g => !yourTurnGames.includes(g)),
+      ];
     });
+  }
+
+  async getCompetedGames() {
+    if (!this.completedGames) {
+      this.completedGames = await this.userApi.completedGames().toPromise();
+    }
   }
 }
