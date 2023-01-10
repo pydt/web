edc340f8b07c606bfd6269c3423e5199bb2e6e3a
Mike Rosack <mrosack@gmail.com>
2023-01-10 12:11:29 -0600

    lint fixes

--- src/app/game/detail/surrender/surrender.component.ts
+++ src/app/game/detail/surrender/surrender.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, OnInit } from "@angular/core";
+import { Component, Input, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { ModalDirective } from "ngx-bootstrap/modal";
 import { Game, GameService } from "pydt-shared";
